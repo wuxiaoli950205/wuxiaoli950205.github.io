3b7a5217f884f7120625ad39ffd2beb880568186
wuxiaoli950205 <1026134151@qq.com>
2016-04-27 10:51:59 +0800

    ddd

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.name "wuxiaoli950205"
+git config --global user.email "1026134151@qq.com"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://wuxiaoli950205.github.io
